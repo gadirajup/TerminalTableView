4df434b8abf48df877083924f400b76b6ef42dc0
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-05-30 22:49:51 -0700

    Initial Commit

---
+++ TerminalTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TerminalTableView
+//
+//  Created by Prudhvi Gadiraju on 5/30/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
