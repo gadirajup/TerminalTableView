050ce63e1ae1be2d2ab552546aa71723377ad101
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-05-30 22:58:00 -0700

    Connected Storyboardr

--- TerminalTableView/ViewController.swift
+++ TerminalTableView/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var output: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
